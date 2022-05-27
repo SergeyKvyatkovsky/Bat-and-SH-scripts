a10dfa935a04e2b23792936ef6c8db0fefd0ef28
Siarhei Kviatkouski <siarhei.kviatkouski@LeverX.local>
2022-05-27 16:48:01 +0300

    add inpput and improve logs.

--- freeIp.bat
+++ freeIp.bat
@@ -2,16 +2,22 @@
 
 chcp 65001
 chcp
+set /P var1="Enter first acted your subnet without dot : "
+set /P var2="Enter second acted your subnet without dot : "
+set /P var3="Enter third acted your subnet without dot : "
 echo #####################################################################
 echo ###############It is Starting to find free IP adresses###############
 echo #####################################################################
 echo Threre are free Ips: >> logFreeIp.txt
+echo Your subnet is : %var1%.%var2%.%var3%.XX
+
+
 for /l %%a in (1,1,255) do (
-    echo It is checking now 10.169.61.%%a
-	for /F "tokens=*" %%g in ('ping 10.169.61.%%a -n 1') do (
-		IF "%%g"=="Request timed out." echo 10.169.61.%%a >> logFreeIp.txt
+    echo It is checking now %var1%.%var2%.%var3%.%%a
+	for /F "tokens=*" %%g in ('ping %var1%.%var2%.%var3%.%%a -n 1') do (
+		IF "%%g"=="Request timed out." echo %var1%.%var2%.%var3%.%%a >> logFreeIp.txt
 	)
-    ping 10.169.61.%%a -n 1 >> logWritingAllActions.txt
+    ping %var1%.%var2%.%var3%.%%a -n 1 >> logWritingAllActions.txt
     )
 echo #####################################################################
 echo ####Operation is finish , you could find free ip in logFreeIp.txt####
